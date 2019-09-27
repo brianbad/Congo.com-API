a096b1050cd2368663a9f3fad5824bd0274ac603
brianbad <brianbad@buffalo.edu>
2019-09-27 17:27:00 -0400

    Initial REST API for user data

---
+++ api/app.js
@@ -0,0 +1,26 @@
+const routes = require('./routes/routes');
+// In order to easily deal with POST and PUT requests to our API, we will 
+// add body parsing middleware. This is where our body-parser module comes 
+// in. body-parser will extract the entire body of an incoming request and 
+// parse it into a JSON object that we can work with.
+const bodyParser = require('body-parser');
+
+// Require packages and set the port
+const express = require('express');
+const port = process.env.port || 3000;
+const app = express();
+
+// Use Node.js body parsing middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+
+routes(app);
+
+// Start the server
+const server = app.listen(port, (error) => {
+    if (error) return console.log(`Error: ${error}`);
+ 
+    console.log(`Server listening on port ${server.address().port}`);
+});
